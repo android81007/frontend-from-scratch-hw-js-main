cf1b4219f4c61fe5ea3b941957335537f55c4408
android81007 <noiser81@ukr.net>
2024-12-18 22:24:04 +0100

    lesson-02/02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -11,12 +11,18 @@ let dice1 = Math.floor(Math.random() * 7)  // your code
 let dice2 = Math.floor(Math.random() * 7)// your code
 let isWinningDouble = false; // your code
 
-/*
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+// if (isWinningDouble) {
+if (dice1 === dice2) {
+  if (dice1 > 3 && dice2 > 3) {
+    console.log('Выигрышный дубль!')
+    isWinningDouble = true;
+  }else {
+    console.log('Не выигрышный дубль.')
+  }
+  
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
