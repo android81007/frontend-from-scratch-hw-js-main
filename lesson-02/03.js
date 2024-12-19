a3a908e8384ff9ae9ba8a76a67e0c9a4f115c830
android81007 <noiser81@ukr.net>
2024-12-19 14:23:05 +0100

    lesson-02/03 change

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -10,19 +10,19 @@ let grade;
 
     
   if (score >= 0 && score <= 49) {
-    grade = "A";
+    grade = "F";
   }
   else if  (score >= 50 && score <= 69) {
-    grade = "B";
+    grade = "D";
   }
   else if (score >= 70 &&  score <= 79) {
     grade = "C";
   }
   else if (score >= 80 && score <= 89) {
-    grade = "D";
+    grade = "B";
   }
   else if (score >= 90  && score <= 100) { 
-    grade = "F";
+    grade = "A";
   }
   else {
     grade = "exit";
