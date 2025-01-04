944854f41a9878573082750e9ee0d912eded5a3c
android81007 <noiser81@ukr.net>
2025-01-04 23:12:09 +0100

    lesson-04/01.js cheng-3

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -14,7 +14,7 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 const array = [22,33,15];
 const element = 15;
-let resulat =  includesElement(array)
+let resulat =  includesElement(array,element);
 function includesElement(array) {
   for (let i = 0; i < array.length; i++) {
     
